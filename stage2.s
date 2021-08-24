728fe06b1d63d091408c192c8818d7c370eb17f7
SecMeant <secmeant@gmail.com>
2021-08-24 18:55:51 +0200

    Jumping into long-mode

--- stage2.s
+++ stage2.s
@@ -2,9 +2,9 @@
 
 [org 0x7e00]
 stage2:
-	;push stage2_win
-	;call puts16
-	;add sp, 2
+	push stage2_win
+	call puts16
+	add sp, 2
 
 	cli
 	lgdt [gdt_addr]
@@ -70,7 +70,6 @@ dd gdt
 ; gdt alignment
 times (32 - ($ - $$) % 32) db 0xcc
 gdt:
-
 ; null segment
 dd 0, 0
 
@@ -81,6 +80,22 @@ dd 0xffff
 dd (2 << 8)|(1 << 12)|(1<<15)|(0xf<<16)|(1<<22)|(1<<23)
 
 gdt_end:
+
+gdt64_addr:
+dw (gdt64_end - gdt64) - 1
+dd gdt64
+
+gdt64:
+; null segment
+dd 0, 0
+
+dd 0xffff
+dd (10 << 8)|(1 << 12)|(1<<15)|(0xf<<16)|(1<<21)|(1<<23)
+
+dd 0xffff
+dd (2 << 8)|(1 << 12)|(1<<15)|(0xf<<16)|(1<<21)|(1<<23)
+
+gdt64_end:
 
 times (32 - ($ - $$) % 32) db 0xcc
 [bits 32]
@@ -133,6 +148,46 @@ start32:
 	call set_stage
 	add esp, 4
 	call print_stage
+
+	mov eax, cr3
+	or eax, PML4 - $$ + 0x7e00
+	mov cr3, eax
+
+	push 4
+	call set_stage
+	add esp, 4
+	call print_stage
+
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+
+	push 5
+	call set_stage
+	add esp, 4
+	call print_stage
+
+	mov ecx, 0xc0000080 ; read EFER
+	rdmsr
+	or eax, 1 << 8 ; Enable IA-32e mode
+	wrmsr
+
+	push 6
+	call set_stage
+	add esp, 4
+	call print_stage
+
+	mov eax, cr0
+	or eax, 1 << 31 | 1 << 0 ; Enable paging
+	mov cr0, eax
+
+	push 7
+	call set_stage
+	add esp, 4
+	call print_stage
+
+	lgdt [gdt64_addr]
+	jmp dword 0x8:start64
 
 	.longmode_setup_failed:
 	jmp $
@@ -215,6 +270,23 @@ check_longmode_available:
 	mov eax, 0
 	setge al
 	ret
+
+; Align PML4 to 4kB
+times (4096 - ($ - $$ + 0x7e00) % 4096) db 0xcc
+PML4:
+dq 1 | 1 << 1 | (PDPTE - $$ + 0x7e00)
+times 511 dq 0
+
+PDPTE:
+dq 1 | 1 << 1 | 1 << 7 | 0 << 30
+times 511 dq 0
+
+start64:
+[bits 64]
+	mov rax, 0xb8000
+	mov rdx, 0x4141414141414141
+	mov [rax], rdx
+	jmp $
 
 stage2_win: db 'Second stage loaded!', 0xd, 0xa, 0
 msg_a20_enabled: db 'A20 line is enabled', 0xd, 0xa, 0
